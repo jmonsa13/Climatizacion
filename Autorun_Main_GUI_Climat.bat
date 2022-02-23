ef7113cac46290021d105582f61cf511eaaa50b8
jmonsalvo <jmonsa13@gmail.com>
2022-02-23 09:59:10 -0500

    Se elimina el archivo Autorun_Main_GUI_Climat.bat
    Se guardan en archivos la data recogida del sql

--- Autorun_Main_GUI_Climat.bat
+++
@@ -1,3 +0,0 @@
-set root=D:\Users\jmonsalvo\Anaconda3\
-call %root%\condabin\activate.bat %root%\envs\Pycharm
-call streamlit run "E:\OneDrive - Corona\03_Proy_ID\03_MACHINE_LEARNING\01_Projects\08_Proyecto_Trazabilidad\04_Climatizacion_Salones_Gr\Main_GUI_IIOT_Climat.py" --server.port 8511
