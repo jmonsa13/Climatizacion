6534c1fa6237ed7e728c4a0093b6472f33fae405
jmonsalvo <jmonsa13@gmail.com>
2022-02-23 09:49:03 -0500

    Se agrega archivo Autorun_Main_GUI_Climat.bat
    Se agrega el archivo que permite ejecutar el servidor de streamlit con una tarea programada de windows

---
+++ Autorun_Main_GUI_Climat.bat
@@ -0,0 +1,3 @@
+set root=D:\Users\jmonsalvo\Anaconda3\
+call %root%\condabin\activate.bat %root%\envs\Pycharm
+call streamlit run "E:\OneDrive - Corona\03_Proy_ID\03_MACHINE_LEARNING\01_Projects\08_Proyecto_Trazabilidad\04_Climatizacion_Salones_Gr\Main_GUI_IIOT_Climat.py" --server.port 8511
